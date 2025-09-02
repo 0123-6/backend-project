558f9ffa1cb916f80c064d839d03ce14f12e600d
90945 <909458209@qq.com>
2025-09-02 21:34:31 +0800

    添加删除角色功能

--- src/role.ts
+++ src/role.ts
@@ -1,6 +1,7 @@
 import {IEntity} from "./interfaceCommon.js";
 import dayjs from "dayjs";
 import app from "./app.js";
+import {userList} from "./user.js";
 
 // 角色信息
 export interface IRole extends IEntity {
@@ -41,6 +42,30 @@ app.post('/role/add', (req, res) => {
 })
 
 // 删除角色
+const deleteRole = (props: IRole)
+  : boolean | string => {
+  const {
+    name,
+  } = props
+  const index = roleList.findIndex(item => item.name === name)
+  if (index === -1) {
+    return '要删除的角色不存在,请检查'
+  }
+  // 有用户在使用,不能删除
+  if (userList.some(user => user.roleList.includes(name))) {
+    return '存在配置该角色的用户,无法删除'
+  }
+
+  roleList.splice(index, 1)
+  return true
+}
+app.post('/role/delete', (req, res) => {
+  const result = deleteRole(req.body)
+  res.json({
+    code: result === true ? 200 : 999,
+    msg: result,
+  })
+})
 
 // 修改角色
 
